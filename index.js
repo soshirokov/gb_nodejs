c1c07c97e739aee748e50960a4da123d366e1600
soshirokov <soshirokov@gmail.com>
2022-03-10 10:44:00 +0300

    l-2

--- index.js
+++ index.js
@@ -1,35 +1,63 @@
-import colors from "colors/safe.js";
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat.js';
+import duration from 'dayjs/plugin/duration.js';
+import EventEmitter from 'events';
 import process from 'process';
 
-let [primeStart, primeEnd] = process.argv.slice(2);
-const palette = [colors.green, colors.yellow, colors.red];
+dayjs.extend(duration);
+/*
+На вход принимает продолжительность таймера в формате ss-mm-hh-dd-mm-yy
+Например, 45-01-02-03-04-05 для 5 лет 4 месяцев 3 дней 2 часов 1 минуты 45 секунд
+*/
+const setTimers = process.argv.slice(2);
+const emitter = new EventEmitter();
 
-if (!Number.isInteger(+primeStart) || !Number.isInteger(+primeEnd)) {
-    console.log(colors.red('Один из аргументов не число'));
-} else {
-    let primeCount = 0;
-
-    if (primeStart < 2) {
-        primeStart = 2;
+class Timer {
+    constructor(timerValue) {
+        this.endTime = this.getTimerEndTime(timerValue);
+        this.timerValue = timerValue;
+        
+        this.interval = setInterval(() => {
+            this.timerTick();
+        }, 1000);
     }
 
-    for (let i = primeStart; i <= primeEnd; i++) {
-        let isPrime = true;
-
-        for (let j = 2; j < i; j++) {
-            if (i % j === 0) {
-                isPrime = false;
-                break;
-            }
+    getTimerEndTime(timerValue) {
+        const timerToArrayOfValues = timerValue.split('-');
+        if (timerToArrayOfValues.length < 6) {
+            throw new Error('Неверный формат продолжительности таймера');
         }
-
-        if (isPrime) {
-            console.log(palette[primeCount % 3](i));
-            primeCount++;
-        }
+        const now = dayjs();
+        const timerDuration = dayjs.duration({
+            seconds: timerToArrayOfValues[0],
+            minutes: timerToArrayOfValues[1],
+            hours: timerToArrayOfValues[2],
+            days: timerToArrayOfValues[3],
+            months: timerToArrayOfValues[4],
+            years: timerToArrayOfValues[5],          
+        });
+        return now.add(timerDuration);
     }
 
-    if (!primeCount) {
-        console.log(colors.red('Не найдено простых чисел в диапазоне'))
+    timerTick() {
+        const duration = dayjs.duration(this.endTime.diff(dayjs()));
+        if (duration.asSeconds() <= 0) {
+            emitter.emit('timerFinish', `Таймер ${this.timerValue} завершился`);
+            clearInterval(this.interval);
+        } else {
+            emitter.emit('timerTick', `До таймера ${this.timerValue} осталось ${duration.format('YY лет MM месяцев DD дней HH:mm:ss')}`);
+        }
     }
 }
+
+setTimers.forEach(inputTimer => {
+    new Timer(inputTimer);
+});
+
+emitter.on('timerFinish', (payload) => {
+    console.log(payload);
+});
+
+emitter.on('timerTick', (payload) => {
+    console.log(payload);
+});
